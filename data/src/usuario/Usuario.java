8fea960a1eeb0e05f7af7132b04fcb0094eeeeb2
Adrian992 <adrian_30escorpio99@hotmail.com>
2020-04-03 22:19:46 -0500

    Laboratorio1

---
+++ data/src/usuario/Usuario.java
@@ -0,0 +1,19 @@
+package usuario;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+	private String nombre;
+	private transient String password;
+	
+	public Usuario(String nombre, String password) {
+		this.nombre = nombre;
+		this.password = password;
+	}
+	
+	public String toString() {
+		String pwd = (password == null) ? "(n/a)" : password;
+		return "Informacion Usuario: \n Nombre: " + nombre + "\n Contraseña: " + pwd;
+	}
+
+}
